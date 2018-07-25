47a1ddbe8e43af2d31f31c719a2d5918722dd496
jc <chan_j@naver.com>
2018-07-24 23:16:29 -0700

    add file dependency changing store format

--- UCI_chatbot_Server/git_graph_drawer.py
+++ UCI_chatbot_Server/git_graph_drawer.py
@@ -5,6 +5,7 @@ file_dir = []
 file_name = []
 file_dependency = {}
 
+# Search All Directory And Append simple file name
 def search_directory(url):
     for (file_path, dir, files) in os.walk(url):
         for filename in files:
@@ -14,7 +15,7 @@ def search_directory(url):
                 file_dir.append(os.path.join(file_path,filename))
                 file_name.append(filename[:len(filename)-3])
 
-
+# File Content Data Reader
 def file_reader(url):
 
     # file open
@@ -25,44 +26,62 @@ def file_reader(url):
 
     return raw_data
 
-
-def generate_file_dependency():
+# Generate Dependency Data
+# 1. File dependency        # { 'file path' : [ dependency file list ], 'file path2' : [ dependency file list ] }
+# 2. Function dependency    # { 'file name + function name' : [ dependency function list ], 'file name + function name' : [ dependency function list ] }
+# 3. Class dependency       # { 'file name + class name' : [ dependency class list ], 'file name + class name' : [ dependency class list ] }
+def generate_dependency():
 
     # Read Each File
     for temp_dir in file_dir:
 
+        # make simple_file_name
+        simple_file_name = str(temp_dir).split('\\')
+        simple_file_name = simple_file_name[len(simple_file_name) - 1]
+        simple_file_name = simple_file_name[:-3]
+
         # Read Raw data
         file_content = file_reader(temp_dir).splitlines()
-
         temp_file_list = []
 
+        # Read Each line
         for file_line in file_content:
 
+            index = 0
             temp_line = file_line.split(' ')
 
-
+            # Read Each token
             for temp_token in temp_line:
+
+                # Generate file dependency : import
                 if temp_token == "import":
-                    import_file = temp_line[1]
+                    import_file = temp_line[index + 1]
 
+                    for temp_file_dir in file_dir:
+                        temp1_file_dir = str(temp_file_dir).split('\\')
+                        temp1_file_dir = temp1_file_dir[len(temp1_file_dir) - 1]
+                        temp1_file_dir = temp1_file_dir[:-3]
 
-                    for temp_file_name in file_name:
-                        if import_file == temp_file_name:
-                            temp_file_list.append(import_file)
-
-                            simple_file_name = str(temp_dir).split('\\')
-                            simple_file_name = simple_file_name[len(simple_file_name)-1]
-                            simple_file_name = simple_file_name[:len(simple_file_name)-3]
-
-                            file_dependency[simple_file_name] = temp_file_list
-                            file_dependency['path'] = root_dir
+                        if import_file == temp1_file_dir:
+                            temp_file_list.append(temp_file_dir)
+                            file_dependency[temp_dir] = temp_file_list
                             break
+
+                # Generate class
+                elif temp_token == "class:":
+                    class_name = temp_line[1][:-1]
+                    print class_name
+
+                # index plus
+                index += 1
 
 
 if __name__ == '__main__':
     search_directory(root_dir)
+
     print file_dir
     print file_name
-    generate_file_dependency()
+
+    generate_dependency()
 
     print file_dependency
