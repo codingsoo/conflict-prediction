49fc16bb2002717df320ca670ce14ed40eff534a
jc <chan_j@naver.com>
2018-08-09 10:10:52 -0700

    work_database.py : modify detect direct conflict logic

--- server_dir/work_database.py
+++ server_dir/work_database.py
@@ -21,7 +21,6 @@ class work_database:
         try:
             # create sql
             sql = "delete from working_table where user_name = '%s'" % user_name
-            print(sql)
 
             # execute sql
             self.cursor.execute(sql)
@@ -37,7 +36,7 @@ class work_database:
     # Detect Direct Conflict
     def detect_direct_conflict(self, project_name, working_list, user_name):
 
-        rows = list()
+        all_rows_list = list()
 
         # search [project_name, file_name] in the working_table
         try:
@@ -51,25 +50,30 @@ class work_database:
                 self.cursor.execute(sql)
                 self.conn.commit()
 
-                rows = self.cursor.fetchall()
-                rows = list(rows)
+                # get rows data
+                rows_list = self.cursor.fetchall()
+                rows_list = list(rows_list)
 
                 # detect direct conflict
-                if(rows != []):
-                    break
+                if(rows_list != []):
+                    for temp_row in rows_list:
+                        all_rows_list.append(temp_row)
 
         except:
             self.conn.rollback()
             print("ERROR : detect direct conflict")
 
         # Direct Conflict
-        if(rows != []):
-            for temp_other_work in rows:
+        if(all_rows_list != []):
+            for temp_other_work in all_rows_list:
                 for temp_user_work in working_list:
 
+                    # Condition
+                    # other_file_name == current_user_file_name
+                    # and
                     # other_logic_name == current_user_logic_name
-                    if(temp_other_work[2] == temp_user_work[1]):
-                        print(temp_other_work[2] + " conflict")
+                    if(temp_other_work[1] == temp_user_work[0] and temp_other_work[2] == temp_user_work[1]):
+                        print(temp_other_work[1] + temp_other_work[2] + " conflict")
 
             result = "conflict"
         else:
