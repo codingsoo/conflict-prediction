2a87f93c5655d7a08b2ee9ba726c3dd0531ffcab
learnitdeep <learnitdeep@gmail.com>
2018-08-13 09:45:58 +0900

    stanford parser example

--- chat_bot_server_dir/user_intent_classifier/intent_classifier.py
+++ chat_bot_server_dir/user_intent_classifier/intent_classifier.py
@@ -1,3 +1,13 @@
-def classfier(sentence):
+from stanfordcorenlp import StanfordCoreNLP
 
+nlp = StanfordCoreNLP('http://localhost', port=9000)
+
+def classfier(sentence):
+    print(nlp.word_tokenize(sentence))
+    print(nlp.pos_tag(sentence))
+    print(nlp.ner(sentence))
+    print(nlp.parse(sentence))
+    print(nlp.dependency_parse(sentence))
     return
+
+classfier("We'll solve it! Thank you!")
