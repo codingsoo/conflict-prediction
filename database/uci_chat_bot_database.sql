e571d7fd5ca40c94360536cf265d96fa2c3d95ee
jc <chan_j@naver.com>
2018-08-19 00:06:21 -0700

    add sql

--- database/uci_chat_bot_database.sql
+++ database/uci_chat_bot_database.sql
@@ -20,7 +20,7 @@ ENGINE=InnoDB
 
 
 /* 3. create table conflict_table */
-CREATE TABLE `conflict_table` (
+CREATE TABLE `direct_conflict_table` (
 	`project_name` VARCHAR(50) NOT NULL,
 	`file_name` VARCHAR(50) NOT NULL,
 	`logic1_name` VARCHAR(50) NOT NULL,
@@ -51,6 +51,31 @@ CREATE TABLE `logic_dependency` (
 	`u` VARCHAR(50) NOT NULL,
 	`v` VARCHAR(50) NOT NULL,
 	`length` INT(11) NOT NULL DEFAULT '0'
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+/* 6. approved_list */
+CREATE TABLE `approved_list` (
+	`project_name` VARCHAR(50) NULL DEFAULT NULL,
+	`approved_file` VARCHAR(50) NULL DEFAULT NULL,
+	INDEX `인덱스 1` (`project_name`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+/* 7 indirect_conflict_table */
+CREATE TABLE `indirect_conflict_table` (
+	`project_name` VARCHAR(50) NOT NULL,
+	`u` VARCHAR(50) NOT NULL,
+	`v` VARCHAR(50) NOT NULL,
+	`length` INT(11) NOT NULL DEFAULT '0',
+	`user1_name` VARCHAR(50) NOT NULL,
+	`user2_name` VARCHAR(50) NOT NULL,
+	`alert_count` INT(11) NOT NULL DEFAULT '1',
+	`log_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
