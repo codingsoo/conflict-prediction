276711e4c3e95262c67f57a7a1a856d32234da80
Sooyoung Baek <qortndud97@naver.com>
2018-09-14 18:20:45 -0700

    exception handling : negative question & question including 'I'

--- chat_bot_server_dir/user_intent_classifier/sentence_type_finder.py
+++ chat_bot_server_dir/user_intent_classifier/sentence_type_finder.py
@@ -1,42 +1,43 @@
 import nltk
 from stanfordcorenlp import StanfordCoreNLP
-import re
+
 
 def sentence_preprocess(_sentence):
-    # User 이름, 파일명 제외하고는 전체 문장 소문자화 하기.
-    punc_list = {"!", "?", ",", ".", "’"}
+    # Change sentence to lower_case except slack code(<@ABCDEFGHI>) and file name(File.py)
+    punc_list = ["!", "?", ",", ".", "’"]
+
+    # Please preserve the order of elements in replace_list.
+    replace_list = [(" please ", " "), (" hey ", " "), (" i think ", " "), (" do not have to ", " should not "),
+                    (" have to ", " should "), (" ought to ", " should "), (" ought not to ", " should not "),
+                    (" can't ", " can not "), (" won't ", " will not "), ("n't ", " not "),
+                    ("'m ", " am "), ("'ve ", " have "), ("'re ", " are ")]
+    # can't -> can not, won't -> will not
+    # shouldn't -> should not, wouldn't -> would not, couldn't -> could not , don't -> do not, haven't -> have not
 
     sentence = _sentence.lstrip("!,.? ").lower()
 
     for punc in punc_list:
+        if punc == "’":
+            sentence = sentence.replace(punc, "'")
         if punc != ".":
             sentence = sentence.replace(punc, " " + punc + " ")
 
     sentence = " " + sentence + " "
-    # User 이름, 파일명 제외하고 전체 문장에서 replace하기.
-    sentence = sentence.replace(" please ", ' ')
-    sentence = sentence.replace(" hey ", ' ')
-    sentence = sentence.replace(" i think ", ' ')
-    sentence = sentence.replace(" have to ", " should ")
-    sentence = sentence.replace("n't ", " not ")
-    sentence = sentence.replace(" don't have to ", " should not ")
-    sentence = sentence.replace(" do not have to ", " should not ")
-    sentence = sentence.replace("'m ", " am ")
-    sentence = sentence.replace("'re ", " are ")
-    sentence = sentence.replace("'ve ", " have ")
 
-    # Sayme preprocessing
-    # sayme 다음에 동사가 나오지 않을 경우 sayme 다 빼냄. (ex : Sayme,.!? Sayme,?.! Could you help me?)
+    for replace_tag in replace_list:
+        sentence = sentence.replace(replace_tag[0], replace_tag[1])
+
+    # Exception handling : Sentence with Sayme (ex : Sayme,.!? Sayme,?.! Could you help me?)
+    # Extract all of "sayme", if the verb doesn't follow "sayme"
     sentence = sentence.lstrip("!.,? ")
     if sentence.startswith("sayme"):
         without_sayme_st = sentence.replace("sayme","").lstrip()
         count = 0
         for punc in punc_list:
-            if without_sayme_st.startswith(punc) == False and punc != "’":
+            if not without_sayme_st.startswith(punc) and punc != "’":
                 count = count + 1
         if count != 4:
             sentence = without_sayme_st.lstrip("!?,. ")
-
 
     word_list = sentence.split()
     for word in word_list:
@@ -47,7 +48,7 @@ def sentence_preprocess(_sentence):
             file_idx = _sentence.lower().find(word)
             modified_word = _sentence[file_idx:file_idx + len(word)]
         else:
-            if word == "i" or word == "i'm" or word == "i've":
+            if word == "i":
                 modified_word = word[0].upper() + word[1:]
             elif word == "sayme":
                 modified_word = "you"
@@ -56,15 +57,13 @@ def sentence_preprocess(_sentence):
         word_list[word_idx] = modified_word
 
     sentence = ' '.join(word_list)
-
     sentence = sentence.strip()
     sentence = " " + sentence + " "
 
     return sentence
 
 
-def is_command(_sentence):
-    nlp = StanfordCoreNLP('http://localhost', port=9000)
+def is_command(_sentence, nlp):
     # sentence 제일 앞에 modal 추가
     sentence = "Must " + _sentence
     pos_tag_list = nlp.pos_tag(sentence)
@@ -104,7 +103,9 @@ def is_suggestion(pos_tag_list):
         else:
             return False
 
-def is_question(parse_list, pos_tag_list):
+def is_question(_sentence, pos_tag_list, nlp):
+    sentence = _sentence.replace(" not ", " ", 1)
+    parse_list = nlp.parse(sentence)
     if "SBARQ" in parse_list or "SQ" in parse_list:
         return True
     else:
@@ -113,7 +114,6 @@ def is_question(parse_list, pos_tag_list):
                 pass
             elif pos_tag[0] == "about":
                 return True
-
                 # pos_idx = pos_tag_list.index((pos_tag[0], pos_tag[1]))
                 # if pos_tag_list[pos_idx + 1][0] == "about":
                 #     return True
@@ -123,26 +123,30 @@ def require_something_sentence(_sentence):
     nlp = StanfordCoreNLP('http://localhost', port=9000)
     sentence = sentence_preprocess(_sentence)
     pos_tag_list = nlp.pos_tag(sentence)
-    parse_list = nlp.parse(sentence)
 
     print("sentence", sentence)
     print("pos_tag_list", pos_tag_list)
-    print("parse_list", parse_list)
+    print("parse_list", nlp.parse(sentence))
 
-    if is_question(parse_list, pos_tag_list):
+    if is_question(sentence, pos_tag_list, nlp):
         for pos_tag in pos_tag_list:
-            if pos_tag[1] == "MD":
-                sentence = sentence.replace(pos_tag[0], "can")
+            if pos_tag[1] == "MD" or pos_tag[0] == "do": # couldn't you do that?
+                sentence = sentence.replace(" "+pos_tag[0]+" ", " can ", 1)
+                pos_idx = pos_tag_list.index((pos_tag[0], pos_tag[1]))
+                if pos_tag_list[pos_idx + 1][0] == "not":
+                    sentence = sentence.replace(" not ", " ", 1)
+                if pos_tag_list[pos_idx + 1][0] == "I" or pos_tag_list[pos_idx + 2][0] == "I":
+                    sentence = sentence.replace(" I ", " you ", 1)
                 break
         return 1, sentence
 
-    elif is_command(sentence):
+    elif is_command(sentence, nlp):
         return 2, sentence
 
     elif is_suggestion(pos_tag_list):
         for pos_tag in pos_tag_list:
             if pos_tag[1] == "MD":
-                sentence = sentence.replace(pos_tag[0], "should")
+                sentence = sentence.replace(pos_tag[0], "should", 1)
                 break
         return 3, sentence
 
